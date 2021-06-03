b9700440dec9277d72a9b2eab5dbed1e5c5ab22a
Jakub Brojacz <kubabr@gmail.com>
2021-06-03 12:34:12 +0200

    PCA

--- src/boruta_tries.py
+++ src/boruta_tries.py
@@ -8,7 +8,6 @@ from sklearn.metrics import balanced_accuracy_score
 data_name = 'digits'
 X = pd.read_csv(f"data/{data_name}_train.data", sep=' ', header=None)
 X = X.drop(columns=[5000])
-X
 Y = pd.read_csv(f"data/{data_name}_train.labels", sep=' ', header=None)
 # X_test = pd.read_(f"data/{data_name}_valid.data")
 X_train, X_val, Y_train, Y_val = train_test_split(X,Y, stratify=Y, random_state=42)
@@ -24,21 +23,28 @@ from boruta import BorutaPy
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np  # initialize Boruta
 from sklearn.ensemble import RandomForestClassifier
+import time
 
-forest = RandomForestClassifier(random_state=0)
 # forest = RandomForestRegressor(max_depth = 5, random_state = 42  # its sooooo slow
+forest = RandomForestClassifier(random_state=0)  # its still slow :-/
 boruta = BorutaPy(
    estimator = forest,
    n_estimators = 'auto',
-   max_iter = 10  # number of trials to perform
+   max_iter = 5  # number of trials to perform
 )  # fit Boruta (it accepts np.array, not pd.DataFrame)
-boruta.fit(np.array(X_train), np.array(Y_train))  # print results
+
+start_time = time.time()
+boruta.fit(np.array(X_train), np.array(Y_train))
+elapsed_time = time.time() - start_time
+print(f'time: {elapsed_time}')
+
 green_area = X_train.columns[boruta.support_].to_list()
 blue_area = X_train.columns[boruta.support_weak_].to_list()
 print('features in the green area:', green_area)
 print('features in the blue area:', blue_area)
-X_tr = boruta.transform(np.array(X_train))
+X_tr = boruta.transform(np.array(X_train), weak=True)
 forest = RandomForestClassifier(random_state=0)
 forest.fit(X_tr, Y_train)
-X_tr = boruta.transform(np.array(X_val))
-print(forest.score(X_tr, Y_val))  # 0.9733333333333334
+X_tr = boruta.transform(np.array(X_val), weak=True)
+print(f'score: {forest.score(X_tr, Y_val)}')  # 0.974
+print(f'features: {len(green_area)+len(blue_area)}')  # 1150
---
+++ src/pca.py
@@ -0,0 +1,37 @@
+from sklearn import set_config
+set_config(display='diagram')  # nwm co to jest
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.metrics import balanced_accuracy_score
+
+data_name = 'digits'
+X = pd.read_csv(f"data/{data_name}_train.data", sep=' ', header=None)
+X = X.drop(columns=[5000])
+Y = pd.read_csv(f"data/{data_name}_train.labels", sep=' ', header=None)
+# X_test = pd.read_(f"data/{data_name}_valid.data")
+X_train, X_val, Y_train, Y_val = train_test_split(X,Y, stratify=Y, random_state=42)
+Y_train = np.ravel(Y_train)
+Y_val = np.ravel(Y_val)
+
+
+from sklearn.feature_selection import SelectKBest, f_classif, VarianceThreshold
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import LinearSVC
+
+num_features = 1000
+
+variance_filter = VarianceThreshold()
+anova_filter = SelectKBest(f_classif, k=1000)
+clf = LinearSVC()
+anova_svm = make_pipeline(variance_filter, anova_filter, clf)
+anova_svm.fit(X_train, Y_train)
+
+from sklearn.metrics import classification_report
+
+y_pred = anova_svm.predict(X_val)
+print(f'Report: {classification_report(Y_val, y_pred)}')
+print(f'Score: {balanced_accuracy_score(Y_val, y_pred)}')  # 0.982
+print(f'Features: {num_features}')  # 1000
